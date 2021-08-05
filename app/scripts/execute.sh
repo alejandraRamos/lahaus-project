0f04b7c2e378848fdc4d04dfcbc54d331486da8f
alejandraRamos <alejandra0907ramos@gmail.com>
2021-08-05 00:53:57 -0500

    changes

--- app/scripts/execute.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 manage.py runserver -h 0.0.0.0 -p 5000
--- app/scripts/psql.sh
+++ app/scripts/psql.sh
@@ -12,22 +12,23 @@ su postgres psql -c 'createdb lahaus;'
 
 service postgresql restart
 
-sudo amazon-linux-extras enable python3.8
-sudo yum install python3.8
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
 
-sudo yum update -y
-sudo yum -y install git
-git clone https://github.com/alejandraRamos/lahaus-project.git
+# sudo amazon-linux-extras enable python3.8
+# sudo yum install python3.8
+# curl -O https://bootstrap.pypa.io/get-pip.py
+# python3 get-pip.py --user
 
-pip3 install flask==1.1.2
-pip3 install flask_sqlalchemy==2.4.1
-pip3 install flask_script
-pip3 install flask_migrate==2.5.2
-pip3 install psycopg2-binary
+# sudo yum update -y
+# sudo yum -y install git
+# git clone https://github.com/alejandraRamos/lahaus-project.git
+
+# pip3 install flask==1.1.2
+# pip3 install flask_sqlalchemy==2.4.1
+# pip3 install flask_script
+# pip3 install flask_migrate==2.5.2
+# pip3 install psycopg2-binary
 
 
-python3 ./lahaus-project/app/manage.py db init
-python3 ./lahaus-project/app/manage.py db migrate
-python3 ./lahaus-project/app/manage.py db upgrade
+# python3 ./lahaus-project/app/manage.py db init
+# python3 ./lahaus-project/app/manage.py db migrate
+# python3 ./lahaus-project/app/manage.py db upgrade
